51cf0c9deae917c59cc79e98fed110525546edd0
test <2750565592@qq.com>
2020-04-21 11:18:48 +0800

    header finish

---
+++ src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import './assets/stylus/reset.css'
+import './assets/stylus/border.css'
+import './assets/stylus/iconfont.css'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="header">
+      <div class="header-left">
+        <span class="iconfont icon-list">&#xe650;</span>
+      </div>
+      <div class="header-middle">
+        <ul>
+          <router-link is="li" to="/" class="header-middle-title">我的</router-link>
+          <router-link is="li" to="/" class="header-middle-title">发现</router-link>
+          <router-link is="li" to="/" class="header-middle-title">云村</router-link>
+          <router-link is="li" to="/" class="header-middle-title">视频</router-link>
+        </ul>
+      </div>
+      <div class="header-right">
+        <span class="iconfont iconfont-search">&#xe612;</span>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .header
+    color: #fff
+    height: .8rem
+    line-height: .8rem
+    background: red
+    display: flex
+    .header-left
+      width: .64rem
+      float: left
+      text-align: center
+      margin: 0 .6rem 0 .2rem
+      .icon-list
+        font-size: .48rem
+    .header-middle
+      flex: 1
+      margin-left: .2rem
+      font-size: .30rem
+      .header-middle-title
+        float: left
+        margin: 0 .3rem
+    .header-right
+      width: .64rem
+      text-align: center
+      margin-right: .2rem
+      .iconfont-search
+        font-size: .48rem
+</style>
